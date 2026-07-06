84b9cabff90cc73040c3544138dad742009867af
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-07-06 12:31:08 +0530

    db created

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from 'dotenv'
+import dbConnect from "./config/db.js";
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT
 app.use(express.json())
 
+// test route
 app.get("/",(req, res)=>{
     res.send("Server Running ✅")
 })
 
 app.listen(port,()=>{
+    dbConnect()
     console.log(`Server is running at http://localhost:${port}`);
 })
