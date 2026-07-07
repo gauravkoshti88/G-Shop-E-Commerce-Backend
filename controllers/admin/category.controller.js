e458ca65c9ec85d0c88220a122fc807aa2492aa0
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-07-07 23:53:02 +0530

    Created category list and update controller

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -1,5 +1,6 @@
+import mongoose from "mongoose";
 import Category from "../../models/admin/categorySchema.js";
-import { uploadToCloudinary } from "../../utils/cloudinaryFunc.js";
+import { updateCloudinaryImage, uploadToCloudinary } from "../../utils/cloudinaryFunc.js";
 
 export const addCategory = async (req, res) => {
     try {
@@ -17,13 +18,13 @@ export const addCategory = async (req, res) => {
         if (categoryExists) {
             return res.status(400).json({
                 success: false,
-                message: "Category already exists"
+                message: `${categoryExists.name} Category already exists`
             })
         }
 
         let image = {
             url: "",
-            public_id:""
+            public_id: ""
         }
 
         if (req.file) {
@@ -41,15 +42,130 @@ export const addCategory = async (req, res) => {
         });
 
         return res.status(201).json({
-            success:true,
-            message:"Category Added Successfully ✅",
+            success: true,
+            message: "Category Added Successfully ✅",
             category
         })
 
-    } catch (error) { 
+    } catch (error) {
         return res.status(500).json({
-            success:false,
+            success: false,
             error: `Add Category Error ${error}`
+        })
+    }
+}
+
+export const categoryList = async (req, res) => {
+    try {
+        const categories = await Category.find().sort({ createdAt: -1 });
+
+        return res.status(200).json({
+            success: true,
+            message: "Get Categories List Successfully",
+            count: categories.length,
+            categories
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: `Category List Error ${error}`
+        })
+    }
+}
+
+export const getSingleCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid Category Id'
+            });
+        }
+
+        const category = await Category.findById(id);
+
+        if (!category) {
+            return res.status(404).json({
+                success: false,
+                message: `Category not found`
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: `Get ${category?.name} Category Successfully`,
+            category
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: `Get Single Category Error ${error}`
+        })
+    }
+}
+
+export const updateCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, description } = req.body;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid Category Id'
+            });
+        }
+
+        const category = await Category.findById(id);
+
+        if (!category) {
+            return res.status(404).json({
+                success: false,
+                message: "Category not found"
+            })
+        }
+
+        if (name && name.trim() !== category.name) {
+            const exists = await Category.findOne({
+                name: name.trim(),
+                _id: { $ne: id },
+            })
+
+            if (exists) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Category already exists",
+                })
+            }
+        }
+
+        if (req.file) {
+            if (category.image.public_id) {
+                const result = await updateCloudinaryImage(req.file.buffer, category.image.public_id, "categories");
+
+                category.image = {
+                    url: result.secure_url,
+                    public_id: result.public_id
+                }
+            }
+        }
+
+        if (name) category.name = name.trim();
+        if (description !== undefined) category.description = description;
+
+        await category.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Category Updated Successfully",
+            category
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: `Update Category Error ${error}`
         })
     }
 }
--- routes/admin/category.routes.js
+++ routes/admin/category.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { adminAuth } from "../../middleware/Auth.js";
-import { addCategory } from "../../controllers/admin/category.controller.js";
+import { addCategory, categoryList, getSingleCategory, updateCategory } from "../../controllers/admin/category.controller.js";
 import upload from "../../middleware/multer.js";
 
 const categoryRouter = express.Router();
 
 categoryRouter.post("/add-category", adminAuth, upload.single("image"), addCategory);
+categoryRouter.get("/category-list", adminAuth, categoryList);
+categoryRouter.get("/category/:id", adminAuth, getSingleCategory);
+categoryRouter.put("/update-category/:id", adminAuth, upload.single("image"), updateCategory);
 
 export default categoryRouter;
